92ad92810d85a017379a339042f6c8d06adca109
ikkhan18 <65544386+ikkhan18@users.noreply.github.com>
2020-10-11 14:37:39 +0100

    Add files via upload

---
+++ Assets/questions.js
@@ -0,0 +1,81 @@
+
+var questions = [{
+  question: "In what year was the first iPhone released?",
+  option1: "2007",
+  option2: "2005",
+  option3: "1999",
+  option4: "2012",
+  answer: "0"
+},
+{
+  question: "The tallest building in the world is located in which city?",
+  option1: "Shanghai Tower",
+  option2: "Ping An Finance Center",
+  option3: "Dubai (Burj Khalifa)",
+  option4: "One World Trade Center",
+  answer: "2"
+},
+{
+  question: "How many actors have played the role of James Bond?",
+  option1: "Nine",
+  option2: "Four",
+  option3: "Six",
+  option4: "Ten",
+  answer: "0"
+},
+{
+  question: "What company is also the name of one of the longest rivers in the world?",
+  option1: "Apple",
+  option2: "Amazon",
+  option3: "Shell",
+  option4: "Fedex",
+  answer: "1"
+},
+{
+  question: "How many hearts does an octopus have",
+  option1: "One",
+  option2: "Three",
+  option3: "Two",
+  option4: "Six",
+  answer: "1"
+},
+{
+  question: "In tennis, what piece of fruit is found at the top of the men's Wimbledon trophy?",
+  option1: "Pineapple",
+  option2: "Strawberry",
+  option3: "Kiwi fruit>",
+  option4: "Apple",
+  answer: "0"
+},  {
+  question: "What is the largest molecule that forms part of a human body?",
+  option1: "Oxygen",
+  option2: "Water",
+  option3: "Calcium",
+  option4: "Chromosome 1",
+  answer: "3"
+},
+{
+  question: "What’s the average surface temperature on Venus?",
+  option1: "10°C ",
+  option2: "460°C",
+  option3: "-70°C",
+  option4: "60°C",
+  answer: "1"
+},
+{
+  question: "What is the name of the biggest technology company in South Korea?",
+  option1: "LG",
+  option2: "Samsung",
+  option3: "Kia Motors",
+  option4: "Hyundai Motor",
+  answer: "1"
+},
+{
+  question: "What is the world’s smallest bird?",
+  option1: "Lesser Goldfinch",
+  option2: "Bee Hummingbird",
+  option3: "Willow Tit",
+  option4: "Weebill",
+  answer: "1"
+}
+];
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# IK-APICQ-111020
-Web APIs Code Quiz Assignment
+Code Quiz script
+================
+
+Overview
+--------
+
+This is multiple questions and answers quiz.  
+
+<img src="sc1.PNG" alt="Quiz main screen"> <br>
+
+You need to select right answer from multiple options of each questions and select right one and press next button to get next question.  
+
+<img src="sc2.PNG" alt="Questions screen"> <br>
+
+The timer will start as you press the start button and will continue until you press Finish button.  After Finish all questions you can see your score by pressing Score button.
+
+<img src="sc3.PNG" alt="Score screen"> <br>
+
+
+## Link
+ 
+ https://ikkhan18.github.io/IK-APICQ-111020/
+
+
+
+## My notes
+
+I found this assignment very challenging. But I learned a lot specialy in javascrip and bootstrip and am happy with the result.  It can be many changes and improvment on quiz concept but will be on later stage.
+
+Thanks
